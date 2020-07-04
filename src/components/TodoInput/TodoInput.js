737e9b9d9a468147bf4207016bcebd09a50df591
高雨 <gaoyu@gaoyudeMBP.lan>
2020-07-04 16:22:14 +0800

    更新笔记

--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class TodoInput extends Component {
     style = {
@@ -6,15 +6,44 @@ class TodoInput extends Component {
         margin: "20px 20px",
         width: "80%"
     }
+
+    constructor() {
+        super();
+        this.state = {
+            inputValue: ""
+        }
+    }
+
+    inputChange = (e) => {
+        console.log(e.currentTarget.value);
+        this.setState({
+            inputValue: e.currentTarget.value
+        })
+    }
+    resetValue = () => {
+        this.setState({
+            inputValue : ""
+        })
+    }
+
     render() {
         return (
-            <div style={{ width: this.style.width, border: this.style.border, margin: this.style.margin }}>
+            <div style={{width: this.style.width, border: this.style.border, margin: this.style.margin}}>
                 <h3>我是input文件</h3>
-                <input type="text" placeholder="请输入" />
                 <br/>
                 {this.props.btnTxt}
                 <br/>
                 {this.props.children}
+                <br/>
+                <input
+                    type="text"
+                    placeholder="请输入"
+                    value={this.state.inputValue}
+                    onChange={this.inputChange}
+                />
+                <button>提交</button>
+                <button onClick={this.resetValue}>重置</button>
+
             </div>
         );
     }
